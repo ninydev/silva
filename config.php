f550bd970a07222c72aab6ef5433458eff103de2
Oleksandr Nykytin <nikitin_a@itstep.org>
2018-06-18 21:09:26 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+namespace AppConfig;
+
+class AppConfig {
+  static $baseDir = '/var/www/clients/client20/web31/web';
+  static $tplDir = '/var/www/clients/client20/web31/web/resources/view';
+}
--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Welcome
+<?php
+
+define('SILVA_START', microtime(true));
+
+include_once ('config.php');
+include_once ('vendor/autoload.php');
+include_once ('Core/boot.php');
+
+echo __DIR__;
+
+?>
